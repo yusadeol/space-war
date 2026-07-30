f119251232473fc9666fabc25b9ed653d34d4def
Yuri Oliveira <yusadeol@gmail.com>
2026-07-30 14:30:28 -0300

    refactor(bullet): encapsulate internals and add BulletGetSize
    Make BulletTexture static to hide implementation details and add
    BulletGetSize returning the new TextureSize type. Reorder function
    declarations and definitions for better readability.

--- include/bullet.h
+++ include/bullet.h
@@ -1,6 +1,7 @@
 #ifndef BULLET_H
 #define BULLET_H
 
+#include "asset.h"
 #include "raylib.h"
 
 constexpr int MAX_SIMULTANEOUS_BULLETS = 50;
@@ -14,11 +15,13 @@ typedef struct Bullet {
     Vector2 position;
 } Bullet;
 
-Texture2D BulletTexture(BulletType type);
 Bullet BulletCreate(BulletType type, Vector2 position);
-void BulletUpdate(Bullet *bullet, float delta);
+
 float BulletGetWidth(const Bullet *bullet);
 float BulletGetHeight(const Bullet *bullet);
+TextureSize BulletGetSize(const Bullet *bullet);
+
+void BulletUpdate(Bullet *bullet, float delta);
 void BulletDraw(const Bullet *bullet);
 
 #endif
--- src/bullet.c
+++ src/bullet.c
@@ -2,7 +2,7 @@
 #include "asset.h"
 #include <raylib.h>
 
-Texture2D BulletTexture(BulletType type) {
+static Texture2D BulletTexture(BulletType type) {
     switch (type) {
     case BULLET_TYPE_PULSE:
         return *AssetGetTexture(TEXTURE_BULLET_PULSE);
@@ -15,18 +15,22 @@ Bullet BulletCreate(BulletType type, Vector2 position) {
     return (Bullet){.texture = BulletTexture(type), .position = position};
 }
 
-void BulletUpdate(Bullet *bullet, float delta) {
-    float move_step = BULLET_SPEED * delta;
-
-    bullet->position.x += move_step;
-}
-
 float BulletGetWidth(const Bullet *bullet) {
     return bullet->texture.width * BULLET_SCALE;
 }
 
 float BulletGetHeight(const Bullet *bullet) {
     return bullet->texture.height * BULLET_SCALE;
+}
+
+TextureSize BulletGetSize(const Bullet *bullet) {
+    return (TextureSize){bullet->texture.width, bullet->texture.height};
+}
+
+void BulletUpdate(Bullet *bullet, float delta) {
+    float move_step = BULLET_SPEED * delta;
+
+    bullet->position.x += move_step;
 }
 
 void BulletDraw(const Bullet *bullet) {
